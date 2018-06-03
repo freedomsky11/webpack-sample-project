fb43ebc28f939b6bde6be77aa5dbf63fac168b9f
freedomsky <hanwei_1989@126.com>
2018-06-03 08:53:50 +0800

    change webpack's version from  newest 4.9.1 to 3.5.3 because the guide i used is writted in 2016, and the version in it is 3.5.3

--- app/Greeter.js
+++ app/Greeter.js
@@ -1,6 +1,6 @@
 // Greeter.js
 module.exports = function() {
   let greet = document.createElement('div');
-  greet.textContent = "Hi there and greetings!";
+  greet.textContent = 'Hi there and greetings!';
   return greet;
 };
